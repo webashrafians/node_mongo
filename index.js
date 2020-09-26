bf55d1f33090b875a29fe08e9bbe3dd4d761ea8e
Md. Ashraful Alam <webashrafians@gmail.com>
2020-09-26 20:48:17 +0600

    medile ware

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
-
+const cors = require('cors');
 
 
 const app = express();
 
+app.use(cors());
+
+
+
+const fruits = ['apple', 'banana', 'orange', 'watermelon'];
+const users = ['Jamal', 'kamal', 'bamal', 'Dhamal', 'tomal', 'vamal'];
 
 
 app.get('/', (req, res) => {
-    // res.send("Thank you fr Calling me");
-    res.send("I knw how to open NODE ... YAY"); 
-})
+    // res.send("I knw how to open NODEMON Run ... YAY"); 
+    const fruit = {
+        product: 'ada',
+        price : 220
+    }
+    res.send(fruit);
+});
 
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit: 'Banana', qty: 1000, price: 15000});
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.short);
+    const name = users[id];
+    res.send({id, name});
+});
 
 //server run
-app.listen(3000, () => console.log("Listening to port : 300"));
+app.listen(4200, () => console.log("Listening to port : 4200"));
